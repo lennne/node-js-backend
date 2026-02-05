e3bc76c5062bdcf4f972f333601b4d5d06035b47
lennne <kobiansah@gmail.com>
2026-02-05 12:44:43 -0330

    feat(express): implement custom middleware and request logging
    - Added requestLogger middleware to track method, URL, and User-Agent
    
    - Implemented addTimeStamp middleware to augment the request object
    
    - Created errorHandler.js to centralize application error logic
    
    - corrected npm scripts to follow standard 'start' vs 'dev' conventions

---
+++ 18. express-concepts/middleware/customMiddleware.js
@@ -0,0 +1,19 @@
+const requestLogger = (req, res, next) => {
+    const timeStamp = new Date().toISOString();
+    console.log(req)
+    const method = req.method;
+    const url = req.url;
+    const userAgent = req.get('User-Agent')
+    console.log(`[${timeStamp}]  ${method} ${url} - ${userAgent}`);
+    
+    //call this function to go to the next middleware function
+    next()
+}
+
+const addTimeStamp = (req, res, next) => {
+    //adding a new property to the request object
+    req.timeStamp = new Date().toISOString()
+    next()
+}
+
+module.exports = { requestLogger, addTimeStamp }
--- 18. express-concepts/server.js
+++ 18. express-concepts/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors');
 const configureCors = require('./config/corsConfig');
+const { requestLogger, addTimeStamp } = require('./middleware/customMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//express logger middleware
+app.use(requestLogger)
+app.use(addTimeStamp)
 
 app.use(configureCors())
 // express json middleware
